5f19e8de9e053800eb020bb04f95f0837b7fe527
James Mason <jmason@alumni.reed.edu>
2016-01-08 12:38:47 -0800

    Added very minimal bitmap file parsing.

--- executeBitmapxForm.js
+++ executeBitmapxForm.js
@@ -1,3 +1,6 @@
 var reader = require(__dirname + '/lib/bitmap_data_reader.js').readBitmap;
 
 reader(__dirname + '/img/bitmap1.bmp');
+
+var bitmap = require(__dirname + '/lib/bitmap_data_reader.js').getBitmap;
+bitmap = bitmap(__dirname + '/img/bitmap1.bmp');
